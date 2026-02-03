1017688c046fde5f4e07889e64247f15d3b38739
DiogoFabricioAG <diogo.abregu.g@uni.pe>
2026-02-03 18:50:23 -0500

    fix: cms schema

---
+++ cms/labora-cms/src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260203_234526 from './20260203_234526';
+
+export const migrations = [
+  {
+    up: migration_20260203_234526.up,
+    down: migration_20260203_234526.down,
+    name: '20260203_234526'
+  },
+];
--- cms/labora-cms/src/payload.config.ts
+++ cms/labora-cms/src/payload.config.ts
@@ -32,6 +32,7 @@ export default buildConfig({
     pool: {
       connectionString: process.env.DATABASE_URL || '',
     },
+    schema: 'public',
   }),
   sharp,
   plugins: [],
